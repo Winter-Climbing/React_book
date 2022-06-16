5037a11dc63780b758c44fcb45a3e47ed4080591
Gose <97828478+Winter-Climbing@users.noreply.github.com>
2022-06-16 22:09:26 +0900

    class
    공부 깔짝

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import "./App.css";
+// import './App.css';
+// import { Component } from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = "리액트";
+// import './App.css';
+// function App() {
+//   const name = '리액트';
+//   return (
+//     <>
+//       <div className="react">{name}</div>;
+//       <input />
+//     </>
+//   );
+// }
+
+// import { Component } from 'react';
+// // 클래스형 컴포넌트
+// class App extends Component {
+//   render() {
+//     const name = 'react';
+//     return (
+//       <>
+//         <div className="react">{name}</div>
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
   return (
-    <>
-      <div className="react">{name}</div>;
-      <input />
-    </>
+    <div>
+      <MyComponent />
+    </div>
   );
-}
+};
 
 export default App;
