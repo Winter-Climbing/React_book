c6f2285e6eb76a31a434945506b75af3805dd2c3
Winter-Climbing <97828478+Winter-Climbing@users.noreply.github.com>
2022-06-18 20:54:10 +0900

    ch4.EventPractice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-// import './App.css';
-// import { Component } from 'react';
-// import MyComponent from './MyComponent';
-import Counter from './Counter';
-import Say from './Say';
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
-
-// import './App.css';
-// function App() {
-//   const name = '리액트';
-//   return (
-//     <>
-//       <div className="react">{name}</div>;
-//       <input />
-//     </>
-//   );
-// }
-
-// import { Component } from 'react';
-// // 클래스형 컴포넌트
-// class App extends Component {
-//   render() {
-//     const name = 'react';
-//     return (
-//       <>
-//         <div className="react">{name}</div>
-//       </>
-//     );
-//   }
-// }
-
-// const App = () => {
-//   return (
-//     <MyComponent name={'React'} favoriteNumber={1}>
-//       리액트
-//     </MyComponent>
-//   );
-// };
 
 export default App;
